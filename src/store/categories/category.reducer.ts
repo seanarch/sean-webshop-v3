5ca12a539c420dc3955540d90d367e21eb662206
Sean <seanzzz@Seans-Mac-Studio.local>
2022-12-06 13:50:34 -0700

    Change categories to use typescript

---
+++ src/store/categories/category.reducer.ts
@@ -0,0 +1,49 @@
+import { Category } from "./category.types";
+import { AnyAction } from "redux";
+import {
+  fetchCategoriesStart,
+  fetchCategoriesSuccess,
+  fetchCategoriesFailed,
+} from "./category.action";
+
+export type CategoriesSate = {
+  readonly categories: Category[];
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
+export const CATEGORIES_INITIAL_STATE: CategoriesSate = {
+  categories: [],
+  isLoading: false,
+  error: null,
+};
+
+export const categoriesReducer = (
+  state = CATEGORIES_INITIAL_STATE,
+  action = {} as AnyAction
+) => {
+  if (fetchCategoriesStart.match(action)) {
+    return { ...state, isLoading: true };
+  }
+
+  if (fetchCategoriesSuccess.match(action)) {
+    return { ...state, categories: action.payload, isLoading: false };
+  }
+
+  if (fetchCategoriesFailed.match(action)) {
+    return { ...state, error: action.payload, isLoading: false };
+  }
+
+  return state;
+
+  // switch (action.type) {
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+  //     return { ...state, isLoading: true };
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+  //     return { ...state, categories: action.payload, isLoading: false };
+  //   case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+  //     return { ...state, error: action.payload, isLoading: false };
+  //   default:
+  //     return state;
+  // }
+};
